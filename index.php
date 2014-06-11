6a145bec1e0aacd1715a8bf9976b0d49f6aa9fc8
oliverde8 <oliverde8@gmail.com>
2014-06-11 22:33:57 +0200

    Index XD

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+define('OWEB_DIR_MAIN', 'vendor/oliverde8/oweb-framework/OWeb/');
+define('OWEB_CONFIG', 'config.ini');
+
+define('OWEB_DEFAULT_PAGE', 'home');
+define('OWEB_DIR_PAGES', "pages");
+define('OWEB_DIR_FAPI', "fapi");
+
+define('OWEB_HTML_DIR_CSS', 'css/');
+define('OWEB_HTML_DIR_JS', 'js/');
+define('OWEB_DIR_DATA', 'var/');
+define('OWEB_WWW_DATA', 'var/');
+define('OWEB_HTML_URL_IMG', 'img/');
+
+//This is to fix a bug that needs to be fixed in OWeb
+define('OWEB_DIR_VIEWS', 'src/views');
+
+
+require_once 'vendor/oliverde8/oweb-framework/OWeb/OWeb.php';
+if (!isset($_SERVER['REMOTE_ADDR']))
+    $_SERVER['REMOTE_ADDR'] = "";
+
+$Oweb = new OWeb\Oweb($_GET, $_POST, $_FILES, $_COOKIE, $_SERVER, $_SERVER['REMOTE_ADDR']);
+
+$Oweb->registerNameSpace('Controller', 'src/controllers');
+$Oweb->registerNameSpace('View', 'src/views');
+$Oweb->registerNameSpace('Model', 'src/models');
+$Oweb->registerNameSpace('Extension', 'src/extensions');
+$Oweb->registerNameSpace('Page', 'src/pages');
+
+$Oweb->init();
+$Oweb->display();
+
+?>
