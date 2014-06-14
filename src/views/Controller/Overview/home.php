f395d2a8bcb48ef6d0908d1df94478b7a09a7d13
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-06-14 11:08:17 +0300

    overview ready

--- src/views/Controller/Overview/home.php
+++ src/views/Controller/Overview/home.php
@@ -1,24 +1,13 @@
-<div class="uk-width-2-10" id='tabmenu'>
-    <!-- tabmenu -->
-    <ul class="side-nav">
-        <li class="side-active"><a href="#">Server</a></li>
-        <li><a href="#">Players</a></li>
-        <li><a href="#">Maps</a></li>
-
-    </ul>
-
-</div>
-<div class="uk-width-8-10">
-    <?php foreach ($this->serverIds as $id) : ?>
-
+<div class="uk-width-1-1">
+    <h2><?php echo $this->l('Servers Online') ?> </h2>
+    
+    <?php foreach ($this->serverIds as $id) : ?>   
 	<?php
         $this->loadWithAjax('Widgets\Server\Display&id='.$id, $this->timeout);
 	/*\OWeb\manage\SubViews::getInstance()->getSubView('\Controller\Widgets\Server\Display')
 		->addParams('id', $id)
 		->display();*/
 	?>
-
     <?php endforeach; ?>
-
 </div>
 
--- src/views/Controller/Widgets/Server/Display.php
+++ src/views/Controller/Widgets/Server/Display.php
@@ -3,36 +3,43 @@
  * @var Model\Server\Data $data
  */
 $data = $this->data;
-
-
+if ($data->isConnected == 1) {
 ?>
 
-<div class="uk-panel uk-panel-box" id="overview-server-<?php echo $this->id; ?>" > 
-    <div class="uk-panel-title  serverName ">
-	<?php echo $this->parseColors($data->server->name) ?>
-    </div>
-    <div class="uk-panel-badge ladder">
-        <?php echo ($data->server->ladderServerLimitMin / 1000) . " - " . ($data->server->ladderServerLimitMax / 1000) . "k"; ?>
-    </div>
-    <table>
-        <tbody>
-            <tr>
-                <td colspan="3" class="currentMap">
-                    <?php echo $this->parseColors($data->currentMap->name).' '.$this->l('by').' '.$this->parseColors($data->currentMap->author) ?>
-                </td>
-            </tr>
-            <tr>
-                <td class="uk-width-1-3 players">
-                    <?php echo count($data->players) .'/'.$data->server->currentMaxPlayers; ?>
-                </td>
-                <td class="uk-width-1-3 spectators">
-                    <?php echo count($data->spectators) .'/'.$data->server->currentMaxPlayers; ?>
-                </td>
-                <td class="uk-width-1-3 mapCount">
-                    <?php echo count($data->maps); ?>
-                </td>
-            </tr>
 
-        </tbody>
-    </table>
-</div>
+<a href="#test" id="overview-server-<?php echo $this->id; ?>">
+    <div class="uk-panel uk-panel-box panel-serverOverview">    
+	<div class="uk-panel-title  serverName ">
+	    <?php echo $this->parseColors($data->server->name) ?>
+	</div>
+	<div class="uk-panel-badge ladder">
+	    <?php echo ($data->server->ladderServerLimitMin / 1000) . " - " . ($data->server->ladderServerLimitMax / 1000) . "k"; ?>
+	</div>
+	<table width="100%">
+	    <tbody>
+		<tr>
+		    <td colspan="3" class="currentMap">
+			<?php echo $this->parseColors($data->currentMap->name) . ' ' . $this->l('by') . ' ' . $this->parseColors($data->currentMap->author) ?>
+		    </td>
+		</tr>
+		<tr>
+		    <td class="uk-width-1-3 players">                  
+			<i class="uk-icon-user"></i>
+			<?php echo count($data->players) . '/' . $data->server->currentMaxPlayers; ?>
+		    </td>
+		    <td class="uk-width-1-3 spectators">
+			<i class="uk-icon-eye"></i>
+			<?php echo count($data->spectators) . '/' . $data->server->currentMaxPlayers; ?>
+		    </td>
+		    <td class="uk-width-1-3 mapCount">
+			<i class="uk-icon-flash"></i>
+			<?php echo count($data->maps); ?>
+		    </td>
+		</tr>
+	    </tbody>
+	</table>
+    </div>
+</a>
+<?php
+}
+?>
